2878b2edb732630e844fe86592bf3d90c734b175
Bufi Leo <bufi.leo@gmail.com>
2022-02-16 16:23:18 +0100

    rails yelp-mvp ok

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
+  end
+
+  # def new
+  #   @review = Review.new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  # end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
